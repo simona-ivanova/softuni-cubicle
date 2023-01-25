307cf28167e8bc163584fd8b3457eef8412e6075
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-01-25 17:32:12 +0200

    Modify static model save

--- src/controllers/cubeContoller.js
+++ src/controllers/cubeContoller.js
@@ -9,7 +9,7 @@ exports.postCreateCube = (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body;
     let cube = new Cube(name, description, imageUrl, difficultyLevel);
 
-    Cube.save(cube);
+    cube.save(cube);
 
     res.redirect('/');
 };
