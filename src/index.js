ebb0c59560f420f2f1308ff7c4de241e68d29f43
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-01-30 17:15:27 +0200

    Change config path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const routes = require('./routes');
-const config = require('./config');
+const config = require('./config/index');
 const setupViewEngine = require('./config/viewEngine');
 
 
