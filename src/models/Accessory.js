2e1607383ae9cfc5f7109ce23ee75e294cfc3822
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-01-31 18:06:01 +0200

    Add accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require('mongoose');
+const mongoosed = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLenght: 50,
+    },
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory; 
