14a43dabb652cc6d199ea203457beef4fdb524fb
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-02-04 17:50:03 +0200

    Register user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const authService = require('../servieces/authService');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
@@ -8,14 +10,22 @@ router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
     if (password !== repeatPassword) {
+        return res.redirect('/404');
+    }
+
+    const existingUser = await authService.getUserByUsername(username);
+
+    if (existingUser) {
         return res.status(404).end();
     }
 
-    
+    const user = await authService.register(username, password);
+
+    res.redirect('/login');
 });
 
 module.exports = router;
