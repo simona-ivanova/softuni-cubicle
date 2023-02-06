8324ecc5f2ec2fcf04f25bac7abf08735d5bcf4b
Simona Ivanova <116807754+simona-ivanova@users.noreply.github.com>
2023-02-06 12:26:20 +0200

    Show edit cube page
    Add button on details page

--- src/controllers/cubeContoller.js
+++ src/controllers/cubeContoller.js
@@ -1,5 +1,7 @@
 const Cube = require('../models/Cube');
 const Accessory = require('../models/Accessory');
+const cubeService = require('../servieces/cubeService');
+const cubeUtils = require('../utils/cubeUtils');
 
 exports.getCreateCube = (req, res) => {
     res.render('create');
@@ -39,4 +41,18 @@ exports.postAttachAccessory = async (req, res) => {
     await cube.save();
 
     res.redirect(`/cubes/${cube._id}/details`);
+};
+
+exports.getEditCube = async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const difficultyLevels = cubeUtils.generateDifficultyLevels(cube.difficultyLevel);
+
+    res.render('cube/edit', { cube, difficultyLevels });
+};
+
+exports.getDeleteCube = async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const difficultyLevels = cubeUtils.generateDifficultyLevels(cube.difficultyLevel);
+
+    res.render('cube/delete', { cube, difficultyLevels });
 };
